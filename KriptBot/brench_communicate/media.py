c3335f088817e8b16ea007cbaa4a60e7c3d32922
Егор Кучук <58609689+KriptYashka@users.noreply.github.com>
2021-12-08 23:45:54 +0300

    Update media.py

--- KriptBot/brench_communicate/media.py
+++ KriptBot/brench_communicate/media.py
@@ -7,7 +7,7 @@ from KriptBot.view.menu import get_default_menu
 def send_photo(message, bot):
     user_id = message.from_user.id
     bot.send_message(user_id, dialog.send_photo(), reply_markup=get_default_menu())
-    path = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.path.join("\\KriptBot\\media\\photo"))
+    path = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.path.join("/KriptBot/media/photo"))
     res = random.choice(os.listdir(path))
     p = os.path.join(path, res)
     photo = open(p, "rb")
