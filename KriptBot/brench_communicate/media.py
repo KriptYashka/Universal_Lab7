e881d72234d3a4856f6b817fe21cfcbc88f69af6
Егор Кучук <58609689+KriptYashka@users.noreply.github.com>
2021-12-08 23:41:55 +0300

    OS path for many os

--- KriptBot/brench_communicate/media.py
+++ KriptBot/brench_communicate/media.py
@@ -7,13 +7,9 @@ from KriptBot.view.menu import get_default_menu
 def send_photo(message, bot):
     user_id = message.from_user.id
     bot.send_message(user_id, dialog.send_photo(), reply_markup=get_default_menu())
-    path = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "\\KriptBot\\media\\photo"
+    path = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.path.join("\\KriptBot\\media\\photo"))
     res = random.choice(os.listdir(path))
     p = os.path.join(path, res)
     photo = open(p, "rb")
     bot.send_photo(user_id, photo)
 
-
-# photo = os.listdir(os.curdir)
-# os.chdir("../KriptBot/brench_communicate/media/photo")
-# print(os.getcwd())
